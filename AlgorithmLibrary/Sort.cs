ffa1306de16b67a6c1457b182749fef5a3d463a5
eroliver <eroliver27@hotmail.com>
2021-04-08 14:30:45 -0400

    Wrote SelectionSort method for lists of strings.

--- AlgorithmLibrary/Sort.cs
+++ AlgorithmLibrary/Sort.cs
@@ -1,18 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace AlgorithmLibrary
 {
     public class Sort
     {
+        public int SmallestValue(List<string> stringList)
+        {
+            string smallestValue = null;
+            int smallestIndex = 0;
+
+            foreach(int index in Enumerable.Range(0, (stringList.Count - 1)))
+            {
+                if(String.Compare(stringList[index], smallestValue) < 0)
+                {
+                    smallestValue = stringList[index];
+                    smallestIndex = index;
+                }
+            }
+
+            return smallestIndex;
+        }
         public List<string> SelectionSort(List<string> unsortedList)
         {
-            List<string> sortedString = null;
+            List<string> sortedList = new List<string>();
 
+            foreach(int index in Enumerable.Range(0, (unsortedList.Count)))
+            {
+                int smallest = SmallestValue(unsortedList);
+                sortedList.Add(unsortedList[smallest]);
+                unsortedList.RemoveAt(smallest);
+            }
 
-
-            return sortedString;
+            return sortedList;
         }
     }
 }
