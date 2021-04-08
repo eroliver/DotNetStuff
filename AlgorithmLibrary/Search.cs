55becdda2d434ab0b83045074737ae8036919a6c
Eric Oliver <eroliver27@hotmail.com>
2021-04-08 01:33:21 -0400

    added the sort class as a new file

--- AlgorithmLibrary/Search.cs
+++ AlgorithmLibrary/Search.cs
@@ -34,4 +34,5 @@ namespace AlgorithmLibrary
             return -1;
         }
     }
+
 }
---
+++ AlgorithmLibrary/Sort.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmLibrary
+{
+    public class Sort
+    {
+        public List<string> SelectionSort(List<string> unsortedList)
+        {
+            List<string> sortedString = null;
+
+
+
+            return sortedString;
+        }
+    }
+}
